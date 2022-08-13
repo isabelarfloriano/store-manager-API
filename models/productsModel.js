07606242f83dc8f5e1f2a86c41f8f0ae031e7471
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-13 18:00:57 -0300

    feat: add getById function to productsModel

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,17 +1,23 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [books] = await connection.execute(
-    'SELECT * FROM StoreManager.products ORDER BY id ',
-);
+  const [products] = await connection.execute(
+    'SELECT * FROM StoreManager.products ORDER BY id ASC;',
+  );
 
-  return books.map(({ id, title, author_id }) => ({
-    id,
-    title,
-    authorId: author_id,
-  }));
+  return products;
+};
+
+const getById = async (productId) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id=?;';
+  const [products] = await connection.execute(query, [productId]);
+
+  if (products.length === 0) return null;
+
+  return products;
 };
 
 module.exports = {
   getAll,
+  getById,
 };
