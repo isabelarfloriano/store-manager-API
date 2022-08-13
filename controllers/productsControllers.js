41394f647dd8aad32d00d2f1864bff365b098e11
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-13 18:56:32 -0300

    feat: add getById to productsControllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -6,6 +6,17 @@ const getAll = async (_req, res) => {
   return res.status(200).json(products);
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+
+  const products = await Products.findById(id);
+
+  if (products.error) return next(products.error);
+
+  return res.status(200).json(products);
+};
+
 module.exports = {
   getAll,
+  getById,
 }; 
