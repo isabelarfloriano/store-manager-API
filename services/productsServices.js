e32d691437a783d3d1b95e9c24c53e454c85e897
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-13 18:31:33 -0300

    feat: add getById function to productsServices

--- services/productsServices.js
+++ services/productsServices.js
@@ -2,6 +2,23 @@ const Products = require('../models/productsModel');
 
 const getAll = async () => Products.getAll();
 
+// Usando de base o exercicio do course 23.2 para o lógica de ERRO
+const getById = async (productId) => {
+  const products = await Products.getById(productId);
+
+  if (!products) {
+    return {
+      error: {
+        code: 'notFound',
+        message: 'Product not found',
+      },
+    };
+  }
+
+  return products;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
