1b756b821d2cdc3cf3b29462f3a092f96dc8fe5a
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-15 22:19:18 -0300

    feat: create at productsRoute endpoint POST/

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -6,5 +6,6 @@ const productsRoute = express.Router();
 
 productsRoute.get('/', rescue(productsControllers.getAll));
 productsRoute.get('/:id', rescue(productsControllers.getById));
+productsRoute.post('/', rescue(productsControllers.createProduct));
 
 module.exports = productsRoute;
