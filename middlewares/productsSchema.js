2f07aa1f6045b6e653d3710382a15777ac6e04f6
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-16 17:16:47 -0300

    fix: messages and apply name validation on endpoint POST/products

--- middlewares/productsSchema.js
+++ middlewares/productsSchema.js
@@ -3,8 +3,8 @@ const joi = require('joi');
 const productsSchema = joi.object({
   name: joi.string().min(5).required()
     .messages({
-      'string.min': '400|Name length must be at least {#limit} characters long',
-      'any.required': '422|Name is required',
+      'string.min': '422|"name" length must be at least {#limit} characters long',
+      'any.required': '400|"name" is required',
   }),
 });
 
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const productsControllers = require('../controllers/productsControllers');
+const productsValidation = require('../middlewares/productsValidation');
 
 const productsRoute = express.Router();
 
+const nameValidate = productsValidation.nameValidation;
+
 productsRoute.get('/', rescue(productsControllers.getAll));
 productsRoute.get('/:id', rescue(productsControllers.getById));
-productsRoute.post('/', rescue(productsControllers.createProduct));
+productsRoute.post('/', nameValidate, rescue(productsControllers.createProduct));
 
 module.exports = productsRoute;
