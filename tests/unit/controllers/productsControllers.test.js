dc3ba28b9944f9b4219352fd2a1daff84b3a9a63
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-17 20:03:26 -0300

    feat: test createProduct at productsControllers

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -115,3 +115,31 @@ describe('Test the function that list a product by specific id | CONTROLLERS', (
     expect(next.calledWith(resultExecute.error)).to.be.equal(true);
   })
 });
+
+describe('Test the function that add product to database | CONTROLLERS', () => {
+  afterEach(() => {
+    Sinon.restore();
+  })
+  it('Should be called an object with status 201 just once', async () => {
+    const request = { body: { name: "ProdutoX" } };
+    const response = {};
+
+    response.status = Sinon.stub().returns(response);
+    response.json = Sinon.stub().returns();
+    const resultExecute = {
+      "id": 4,
+      "name": "ProdutoX",
+    }
+    Sinon.stub(productsServices, 'createProduct').resolves(resultExecute);
+
+    await productsControllers.createProduct(request, response);
+    expect(response.json.calledWith(
+    {
+      "id": 4,
+      "name": "ProdutoX",
+    }
+    )).to.be.equal(true);
+    expect(response.status.calledWith(201)).to.be.equal(true);
+    expect(response.status.calledOnce).to.be.true;
+  })
+});
