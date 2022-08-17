3a2b38cd8b130616aefbf5ad29f82f57be02853f
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-16 21:58:31 -0300

    feat: apply Sinon

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -1,10 +1,20 @@
 const { expect } = require('chai');
 const { describe } = require('mocha');
+const Sinon = require('sinon');
 
+const connection = require('../../../models/connection');
 const productsModel = require('../../../models/productsModel');
 
+// Usando de base a mentoria de revisão bloco 23
+
 describe('Test the function that lists all products', () => {
+  afterEach(() => {
+    Sinon.restore();
+  })
   it('Should return an array', async () => {
+    const resultExecute = []
+    Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
     const products = await productsModel.getAll();
     expect(products).to.be.an('array');
   })
