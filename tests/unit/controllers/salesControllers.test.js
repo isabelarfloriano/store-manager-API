c38e09c8ad01b6040cea064944dae5b6638a2bd9
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-19 19:07:55 -0300

    feat: add and adapt getById function tests for sales in the current layer | CONTROLLERS

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -66,3 +66,65 @@ describe('Test the function that lists all sales | CONTROLLERS', () => {
     expect(response.status.calledOnce).to.be.true;
   })
 });
+
+describe('Test the function that list a Sale by specific id | CONTROLLERS', () => {
+  afterEach(() => {
+    Sinon.restore();
+  })
+  it('Should return an object with correct data', async () => {
+    const request = { params: '1' };
+    const response = {};
+    const next = Sinon.stub().resolves();
+    response.status = Sinon.stub().returns(response);
+    response.json = Sinon.stub().returns();
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(salesServices, 'getById').resolves(resultExecute);
+
+    await salesControllers.getById(request, response, next);
+    expect(response.json.calledWith(
+      {
+        "date": "2021-09-09T04:54:29.000Z",
+        "productId": 1,
+        "quantity": 2,
+      }
+    )).to.be.equal(true);
+  })
+  it('Should response with status 200 just once', async () => {
+    const request = { params: '1' };
+    const response = {};
+
+    response.status = Sinon.stub().returns(response);
+    response.json = Sinon.stub().returns();
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(salesServices, 'getById').resolves(resultExecute);
+
+    await salesControllers.getById(request, response);
+    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.status.calledOnce).to.be.true;
+  })
+  it('Checks if the error parameter is passed correctly', async () => {
+    const request = { params: '1234' };
+    const response = {};
+    const next = Sinon.stub().resolves();
+    response.status = Sinon.stub().returns(response);
+    response.json = Sinon.stub().returns();
+    const resultExecute = {
+      error: {
+        code: 'notFound',
+        message: 'Sale not found',
+      },
+    };
+    Sinon.stub(salesServices, 'getById').resolves(resultExecute);
+
+    await salesControllers.getById(request, response, next);
+    expect(next.calledWith(resultExecute.error)).to.be.equal(true);
+  })
+});
