43fc2a3766f89e912484a3073ebfc68cbe38557f
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-19 18:27:13 -0300

    feat: create productsServices.test.js with all getAll tests adapted to current layer | SERVICES

---
+++ tests/unit/services/salesServices.test.js
@@ -0,0 +1,59 @@
+const { expect } = require('chai');
+const { describe } = require('mocha');
+const Sinon = require('sinon');
+
+const salesModel = require('../../../models/salesModel');
+const salesServices = require('../../../services/salesServices');
+
+describe('Test the function that lists all sales | SERVICES', () => {
+  afterEach(() => {
+    Sinon.restore();
+  })
+  it('Should return an array', async () => {
+    const resultExecute = []
+    Sinon.stub(salesModel, 'getAll').resolves([resultExecute]);
+
+    const sales = await salesServices.getAll();
+    expect(sales).to.be.an('array');
+  })
+  it('Array must not be empty', async () => {
+    const resultExecute = []
+    Sinon.stub(salesModel, 'getAll').resolves(resultExecute);
+
+    const sales = await salesServices.getAll();
+    expect(sales).to.be.empty;
+  })
+  it('Array must be empty', async () => {
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(salesModel, 'getAll').resolves(resultExecute);
+
+    const sales = await salesServices.getAll();
+    expect(sales).to.be.not.empty;
+  })
+  it('Array must contain objects', async () => {
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(salesModel, 'getAll').resolves(resultExecute);
+
+    const sales = await salesServices.getAll();
+    expect(sales[0]).to.be.an('object');
+  })
+  it('Objects inside array must contain right properties', async () => {
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(salesModel, 'getAll').resolves(resultExecute);
+
+    const sales = await salesServices.getAll();
+    expect(sales[0]).to.all.keys('date', 'productId', 'quantity');
+  })
+});
