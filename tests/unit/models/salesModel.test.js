7bbce975d29fc75232e6e421745394cd4b65104d
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-19 18:06:45 -0300

    feat: add tests for getAll sales function return. Array contain objects with correct properties | Model

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -27,11 +27,33 @@ describe('Test the function that lists all sales | MODEL', () => {
     const resultExecute = [{
       "date": "2021-09-09T04:54:29.000Z",
       "productId": 1,
-      "quantity": 2
+      "quantity": 2,
     }]
     Sinon.stub(connection, 'execute').resolves(resultExecute);
 
     const sales = await salesModel.getAll();
     expect(sales).to.be.not.empty;
+  })
+  it('Array must contain objects', async () => {
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+    const sales = await salesModel.getAll();
+    expect(sales[0]).to.be.an('object');
+  })
+  it('Objects inside array must contain "id" and "name" properties', async () => {
+    const resultExecute = [{
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2,
+    }]
+    Sinon.stub(connection, 'execute').resolves([resultExecute]);
+
+    const sales = await salesModel.getAll();
+    expect(sales[0]).to.all.keys('date', 'productId', 'quantity');
   })
 });
