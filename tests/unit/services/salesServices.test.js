b8652ed37c1311f787c4764468765cfe6a9eff5b
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-25 20:01:34 -0300

    feat: add addSale function tests in the current layer | SERVICES

--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -4,6 +4,7 @@ const Sinon = require('sinon');
 
 const salesModel = require('../../../models/salesModel');
 const salesServices = require('../../../services/salesServices');
+const productsModel = require('../../../models/productsModel');
 
 describe('Test the function that lists all sales | SERVICES', () => {
   afterEach(() => {
@@ -82,6 +83,74 @@ describe('Test the function that list a sale by specific id | SERVICES', () => {
     Sinon.stub(salesModel, 'getById').resolves(resultExecute);
 
     const sale = await salesServices.getById('1234');
+    expect(sale).to.be.an('object');
+    expect(sale.error).to.include.all.keys('code', 'message');
+  })
+});
+
+describe('Test the function that add a sale to database | SERVICE', () => {
+  afterEach(() => {
+    Sinon.restore();
+  })
+  it('Should return an object with correct data in case of success', async () => {
+    const resultExecute = {
+      "id": 3,
+      "itemsSold": [
+        {
+          "productId": 1,
+          "quantity": 1
+        },
+        {
+          "productId": 2,
+          "quantity": 5
+        }
+      ]
+    }
+    Sinon.stub(salesModel, 'addSale').resolves(resultExecute);
+
+    const sale = await salesServices.addSale([
+      {
+        "productId": 1,
+        "quantity": 1
+      },
+      {
+        "productId": 2,
+        "quantity": 5
+      }
+    ]);
+    expect(sale).to.be.an('object');
+    expect(sale).to.include.all.keys('id', 'itemsSold');
+    expect(sale.itemsSold).to.be.an('array');
+    expect(sale.itemsSold[0]).to.be.not.empty;
+  })
+  it('Should return a error object if the product was not found', async () => {
+    const products = [{
+      "id": 1,
+      "name": "Martelo de Thor",
+    },
+    {
+      "id": 2,
+      "name": "Traje de encolhimento",
+    }]
+    Sinon.stub(productsModel, 'getAll').resolves(products);
+    const resultExecute = [{
+      error: {
+        code: 'notFound',
+        message: 'Product not found',
+      },
+    }]
+    Sinon.stub(salesModel, 'addSale').resolves(resultExecute);
+
+    const sale = await salesServices.addSale([
+      {
+        "productId": 1,
+        "quantity": 1
+      },
+      {
+        "productId": 3,
+        "quantity": 5
+      }
+    ]);
     expect(sale).to.be.an('object');
     expect(sale.error).to.include.all.keys('code', 'message');
   })
