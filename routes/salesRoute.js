a5f586f2fa960120826955eed432bf8437120cff
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-26 20:08:38 -0300

    feat: create endpoint PUT/sales for updateSale

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -11,5 +11,6 @@ salesRoute.post('/', saleValidate, rescue(salesControllers.addSale));
 salesRoute.get('/', rescue(salesControllers.getAll));
 salesRoute.get('/:id', rescue(salesControllers.getById));
 salesRoute.delete('/:id', rescue(salesControllers.deleteSale));
+salesRoute.put('/:id', saleValidate, rescue(salesControllers.updateSale));
 
 module.exports = salesRoute;
